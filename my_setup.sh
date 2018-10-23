b33f4a33fd33ce70f3aa57e30c7085350c9f0300
Wesley Ketchum <wketchum@fnal.gov>
2018-10-22 19:52:51 -0500

    update setup to pull down ubana

--- my_setup.sh
+++ my_setup.sh
@@ -7,4 +7,11 @@ source setup_user_python.sh
 mrb newDev -p
 source localProducts_*/setup
 
+cd $MRB_SOURCE
+mrb g -t feature/wketchum_PrecutsAnaTree ubana
+
+cd $MRB_TOP
+
+mrbslp
+
 #jupyter nbconvert --to script setup_interactive.ipynb
