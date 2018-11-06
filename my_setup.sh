89bd7bc346bbf391829bd6c041597eb738d5491a
Wesley Ketchum <wketchum@fnal.gov>
2018-11-06 09:18:59 -0600

    initial commit

---
+++ my_setup.sh
@@ -0,0 +1,17 @@
+
+source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+
+setup uboonecode v07_09_00 -qe17:prof
+source setup_user_python.sh
+
+mrb newDev -p
+source localProducts_*/setup
+
+#cd $MRB_SOURCE
+#mrb g -t feature/wketchum_PrecutsAnaTree ubana
+
+#cd $MRB_TOP
+
+mrbslp
+
+#jupyter nbconvert --to script setup_interactive.ipynb
